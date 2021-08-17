2f456ad933cb8b170019f29a7c2b28714a20d70b
dpak1999 <deepakkumardash313@gmail.com>
2021-08-17 06:28:12 +0530

    add : navbar component and social icnos

---
+++ src/components/Navbar.js
@@ -0,0 +1,70 @@
+/** @format */
+
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+
+const Navbar = () => {
+  return (
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold tracking-widest cursive"
+          >
+            DEEPAK
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/blog"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Blogs
+          </NavLink>
+          <NavLink
+            to="/project"
+            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
+            Projects
+          </NavLink>
+        </nav>
+
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://www.linkedin.com/in/deepak-kumar-dash-14a293176/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://github.com/dpak1999"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.facebook.com/deepak.dash.529/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
