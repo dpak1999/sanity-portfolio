8b561f5de2683b9bdddeaddcfdb6b102a86c91cb
dpak1999 <deepakkumardash313@gmail.com>
2021-08-17 06:18:12 +0530

    add : react router for routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 /** @format */
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Post from "./components/Post";
+import Project from "./components/Project";
+import SinglePost from "./components/SinglePost";
 
 function App() {
-  return <div>Hello from the app</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/blog/:id" component={SinglePost} />
+        <Route path="/blog" component={Post} />
+        <Route path="/project" component={Project} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
